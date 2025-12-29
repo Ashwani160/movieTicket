c0ba153b8806735d4b87fa306dfd184d682e6363
Ashwani160 <ashwani11002004@gmail.com>
2025-12-29 18:28:13 +0530

    app.jsx

--- client/src/components/custom/BookingCard.jsx
+++ client/src/components/custom/BookingCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '../ui/button';
+import { Button } from '../ui/button.jsx';
 
 const BookingCard = ({ booking }) => {
   const { show, bookedSeats, amount, isPaid, _id } = booking;
