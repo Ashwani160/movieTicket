ed9b32e2d7ca48d0a822ef77cf62a5ee03652dab
Ashwani160 <ashwani11002004@gmail.com>
2025-12-30 21:36:28 +0530

    final

--- server/src/inngest/index.js
+++ server/src/inngest/index.js
@@ -1,74 +1,80 @@
-// import { Inngest } from "inngest";
-// import { User } from "../models/user.model.js";
+import { Inngest } from "inngest";
+import {User} from "../models/user.model.js";
 
-// // Create a client to send and receive events
-// export const inngest = new Inngest({ id: "book-show" });
+// Create a client to send and receive events
+export const inngest = new Inngest({ id: "book-show" });
 
-// // --- 🟢 NEW FUNCTION FOR SESSION CREATED ---
-// const logSessionStart = inngest.createFunction(
-//     { id: "log-session-start" },
-//     { event: "clerk/session.created" },
-//     async ({ event, step }) => {
-//         console.log("hello there");
-//         return { message: "Session started: hello there" };
-//     }
-// );
+//inngest function to save user data from clerk to mongodb
+const syncUserCreation= inngest.createFunction(
+    {id: 'sync-user-from-clerk'},
+    {event: 'clerk/user.created'},
+    async ({ event, step }) => {
+        const {id, first_name, last_name, email_addresses, image_url}=event.data;
+        const userData={
+            _id:id,
+            email: email_addresses[0].email_address,
+            username: `${first_name} ${last_name}`,
+            image:image_url
+        }
+        await User.create(userData);
+    },
+)
 
-// // --- Existing User Sync Functions ---
+const handleUserLogin = inngest.createFunction(
+    { id: "welcome-user-on-login" },
+    { event: "clerk/session.created" }, // Triggered when a new session is created
+    async ({ event, step }) => {
+        const { user_id } = event.data;
 
-// const syncUserCreation = inngest.createFunction(
-//     { id: 'sync-user-from-clerk' },
-//     { event: 'clerk/user.created' },
-//     async ({ event, step }) => {
-//         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-//         const userData = {
-//             _id: id,
-//             email: email_addresses[0].email_address,
-//             username: `${first_name} ${last_name}`,
-//             image: image_url
-//         }
-//         await User.create(userData);
-//     },
-// )
+        // You could also fetch the user from your DB if you want to print their name
+        // const user = await User.findById(user_id);
+        // console.log(`Welcome back, ${user.username}!`);
 
-// const syncUserDeletion = inngest.createFunction(
-//     { id: "delete-user-with-clerk" },
-//     { event: "clerk/user.deleted" },
-//     async ({ event, step }) => {
-//         const { id } = event.data;
-//         await User.findByIdAndDelete(id);
-//     },
-// )
+        console.log(`Welcome! User ${user_id} has just logged in.`);
+        
+        return { message: `Welcome message logged for user ${user_id}` };
+    }
+);
 
-// const syncUserUpdate = inngest.createFunction(
-//     { id: "update-user-from-clerk" },
-//     { event: "clerk/user.updated" },
-//     async ({ event, step }) => {
-//         const { id, first_name, last_name, email_addresses, image_url } = event.data;
-//         const userData = {
-//             _id: id,
-//             email: email_addresses[0].email_address,
-//             username: `${first_name} ${last_name}`,
-//             image: image_url
-//         }
-//         await User.findByIdAndUpdate(id, userData);
-//     }
-// )
+const syncUserDeletion= inngest.createFunction(
+    { id: "delete-user-with-clerk" },
+    { event: "clerk/user.deleted" },
+    async ({ event, step }) => {
+        const {id}=event.data;
+        await User.findByIdAndDelete(id);
+    },
+) 
 
-// const helloWorld = inngest.createFunction(
-//     { id: "hello-world" },
-//     { event: "test/hello.world" },
-//     async ({ event, step }) => {
-//         await step.sleep("wait-a-moment", "1s");
-//         return { message: `Hello ${event.data.email}!` };
-//     },
-// );
+const syncUserUpdate=inngest.createFunction(
+    {id: "update-user-from-clerk"},
+    {event:"clerk/user.updated"},
+    async({event, step})=>{
+        const {id, first_name, last_name, email_addresses, image_url}=event.data;
+        const userData={
+            _id:id,
+            email: email_addresses[0].email_address,
+            username: `${first_name} ${last_name}`,
+            image:image_url
+        }
+        await User.findByIdAndUpdate(id, userData);
+    }
+)
 
-// // --- 🟢 EXPORT UPDATED ARRAY ---
-// export const functions = [
-//     syncUserCreation,
-//     syncUserDeletion,
-//     syncUserUpdate,
-//     helloWorld,
-//     logSessionStart // <--- Added the new function here
-// ];
+
+const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+    await step.sleep("wait-a-moment", "1s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
+
+// Create an empty array where we'll export future Inngest functions
+export const functions = [
+    syncUserCreation,
+    syncUserDeletion,
+    syncUserUpdate,
+    handleUserLogin,
+    helloWorld
+];
